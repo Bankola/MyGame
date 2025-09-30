1734a3ba7ca315b7935564e229454dbd73340b8c
admin <ivanyatrsuh@gmail.com>
2025-09-30 23:52:19 +0300

    Added colors in settings

--- MyGames/Game.c
+++ MyGames/Game.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <windows.h>
 int main() {
     int user_choice = -1;
     int Flag = 1;
@@ -132,7 +133,7 @@ int main() {
                 }
             }
             break;
-        case 2://Back don't work
+        case 2:
             user_choice = -1;
             while (user_choice != 1 && user_choice != 2 && user_choice != 3 && user_choice != 0) {
                 system("cls");
@@ -186,7 +187,47 @@ int main() {
                     }
                     break;
                 case 2:
-                    printf("Colors\n");
+                    user_choice = -1;
+                    while (user_choice != 1 && user_choice != 2 && user_choice != 3 && user_choice != 4 && user_choice != 0) {
+                        system("cls");
+                        printf("**COLORS**\n"
+                        "1.Blue-White system\n"
+                        "2.Green-Black system\n"
+                        "3.Black-Purple\n"
+                        "4.Back\n"
+                        "0.Exit\n");
+                        scanf_s("%d", &user_choice);
+                        switch (user_choice) {
+                        case 1:
+                            system("color 17");
+                            system("cls");
+                            continue;//dont work
+                        case 2:
+                            system("color 20");
+                            system("cls");
+                            continue;//dont work
+                        case 3:
+                            system("color 5");
+                            system("cls");
+                            continue;
+                        case 4:
+                            continue;
+                        case 0:
+                            printf("Completion the program..\n");
+                            system("pause");
+                            Flag = 0;
+                            break;
+                        default:
+                            printf("Incorrect input\n");
+                            system("pause");
+                            system("cls");
+                            continue;
+                        }
+                        if (Flag == 0) {
+                            Flag = 1;
+                            break;
+                        }
+                    }
                     break;
                 case 3:
                     system("cls");
